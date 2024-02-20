e061e17e88d118d484f73927846fd6c9491f5bcc
Javier <javierilite@gmail.com>
2024-02-20 20:20:10 +0100

    Commit automatico %date% %time%

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<?php include("plantilla/header.php"); ?>
+
+<body>
+    <?php include("plantilla/head.php"); ?>
+
+    <main>
+        <!-- Inicio del Contenido -->
+
+    </main>
+
+    <?php include("plantilla/footer.php"); ?>
+</body>
+
+</html>
---
+++ plantilla/header.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand" href="#">
+        <img src="logo.png" width="30" height="30" class="d-inline-block align-top" alt="" />
+        COMARCA COMUNIDAD DE TERUEL
+    </a>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/inicio.html">INICIO</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/eventos.html">EVENTOS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/sobrenosotros.html">SOBRE NOSOTROS</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/ayuda.html">AYUDA</a>
+            </li>
+        </ul>
+    </div>
+</nav>
